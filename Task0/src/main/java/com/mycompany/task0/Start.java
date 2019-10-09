ee1ef3b208739f2472d36f878dfd56046129a858
baccio <baccio@host-131-114-209-104.m.unipi.it>
2019-10-09 21:46:00 +0200

    aggiunte le funzionalità specificate

--- Task0/src/main/java/com/mycompany/task0/Start.java
+++ Task0/src/main/java/com/mycompany/task0/Start.java
@@ -17,47 +17,105 @@ import java.util.Scanner;
  * @author Guggio
  */
 public class Start {
+    static Connection general_conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/clinica", "root", "");
     public static void main(String[] args) throws SQLException{
-        
-        
         try(
-                Connection co = DriverManager.getConnection("jdbc:mysql://localhost:3306/clinica", "root", "");
-                PreparedStatement ps = co.prepareStatement("select codfisc from clinica.paziente where name = ?;");
+                PreparedStatement ps = general_conn.prepareStatement(
+                           "select * from visita where fk_dottore = ? and data = ?;"
+                            );
                 ){
-            ps.setString(1, nome);
+            ps.setString(1, doctor);
+            ps.setString(2, date)
             ResultSet rs = ps.executeQuery();
             while(rs.next()){
                 System.out.println(rs.getString(1));
             }
-            co.close();
         }catch(SQLException e){
             e.printStackTrace();
         }
     }
     
-    public static void showMenu(){
-        Scanner sc = new Scanner(System.in);
-        String type = "";
-        System.out.println("Inserisci \"d\" se sei un dottore, \"p\" se sei un paziente:);");
-        type = sc.nextLine();
-        if(type.equals("d"))
-            choicesDoc();
-        else if(type.equals("p"))
-            choicesPat();
-        else{
-            System.out.println("Input errato, inserisci d o p");
+    public static void getAgenda(String doctor, String date) throws SQLException{
+        try(
+                PreparedStatement ps = general_conn.prepareStatement(
+                           "select fk_paziente "
+                         + "from visita "
+                         + "where fk_dottore = ? and data = ?;");
+                ){
+            ps.setString(1, doctor);
+            ps.setString(2, date);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                System.out.println(
+                        "Patient: " + rs.getString("fk_paziente")
+                );
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
         }
-        System.out.println("Inserisci il comando:");
-        System.out.println("\t1 -> Crea Appuntamento");
-        System.out.println("\t2 -> Cancella Appuntamento");
-        System.out.println("\t3 -> ")
     }
     
-    public static void choicesDoc(){
-        System.out.println("");
+    public static void getMedicals(String doctor, String patient) throws SQLException{
+        try(
+                PreparedStatement ps = general_conn.prepareStatement(
+                           "select data "
+                         + "from visita "
+                         + "where fk_dottore = ? and fk_paziente = ?;");
+                ){
+            ps.setString(1, doctor);
+            ps.setString(2, patient);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                System.out.println(
+                        "Date: " + rs.getString("fk_paziente")
+                );
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
     }
     
-    public static void choicesPat(){
-        
+    public static void newMedical(String type, String date, String doctor, String patient) throws SQLException{
+        try(
+                PreparedStatement ps = general_conn.prepareStatement(
+                           "insert into visita"
+                         + "values "
+                         + "(?,?,?,?);");
+                ){
+            ps.setString(1, type);
+            ps.setString(2, date);
+            ps.setString(3, doctor);
+            ps.setString(4, patient);
+            int rows = ps.executeUpdate();
+            if(rows == 1)
+                System.out.println("Your medical has been correctly inserted");
+            else
+                System.out.println("Something went wrong, check if medical already exists!");
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
     }
+    
+    public static void updateMedical(String oldDate, String newDate, String doctor, String patient) throws SQLException{
+        try(
+                PreparedStatement ps = general_conn.prepareStatement(
+                           "update visita"
+                         + "set data = ? "
+                         + "where fk_dottore = ? and fk_paziente = ? and data = ?");
+                ){
+            ps.setString(1, newDate);
+            ps.setString(2, doctor);
+            ps.setString(3, patient);
+            ps.setString(4, oldDate);
+            int rows = ps.executeUpdate();
+            if(rows == 1)
+                System.out.println("Your medical has been correctly modified");
+            else
+                System.out.println("Something went wrong, check if medical data is correct");
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+    
+    
 }
