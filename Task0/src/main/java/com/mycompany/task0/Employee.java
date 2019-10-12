ef67ce8d23065cb399ba02f7285c436d50c455d0
baccio <baccio@localhost>
2019-10-12 20:28:50 +0200

    Doctor ora estende User, Employee terminata

--- Task0/src/main/java/com/mycompany/task0/Employee.java
+++ Task0/src/main/java/com/mycompany/task0/Employee.java
@@ -17,54 +17,54 @@ import java.sql.SQLException;
  */
 public class Employee extends User{
    
- 
+
     public Employee(String name, String surname) throws SQLException{
         super(name,surname,"e");
     }
     
-    
-    public boolean addMedical(String pat_name,String pat_surname,String doc_name, String doc_surname, String date) throws SQLException {
-        Patient pat = new Patient(pat_name, pat_surname);
-        Doctor doc = new Doctor(doc_name, doc_surname);
-        if(pat.getIdCode() == 0 || doc.getIdCode() == 0)
+
+    public boolean addMedical(Patient patient,Doctor doctor, String date) throws SQLException {
+        int patCode=patient.getIdCode();
+        int docCode=doctor.getIdCode();
+        if(patCode == 0 || docCode == 0)
             return false;
         PreparedStatement ps = Interface.connection.prepareStatement(
                            "Insert into medical" 
                          + "values(?,?,?,true);");
-        ps.setInt(1, doc.getIdCode());
-        ps.setInt(2, pat.getIdCode());
+        ps.setInt(1, docCode);
+        ps.setInt(2, patCode);
         ps.setString(3, date);
         int rs = ps.executeUpdate();
         return rs == 1;
     }
     
-    public boolean dropMedical(String pat_name,String pat_surname,String doc_name, String doc_surname, String date) throws SQLException {
-        Patient pat = new Patient(pat_name, pat_surname);
-        Doctor doc = new Doctor(doc_name, doc_surname);
-        if(pat.getIdCode() == 0 || doc.getIdCode() == 0)
+    public boolean dropMedical(Patient patient,Doctor doctor, String date) throws SQLException {
+        int patCode=patient.getIdCode();
+        int docCode=doctor.getIdCode();
+        if(patCode == 0 || docCode == 0)
             return false;
         PreparedStatement ps = Interface.connection.prepareStatement(
                            "delete from medical" 
                          + "where fk_doctor = ? and fk_patient = ? and medical_date = ?;");
-        ps.setInt(1, doc.getIdCode());
-        ps.setInt(2, pat.getIdCode());
+        ps.setInt(1, docCode);
+        ps.setInt(2, patCode);
         ps.setString(3, date);
         int rs = ps.executeUpdate();
         return rs == 1;
     }
     
     //approved = 0 -> rejected, else approved
-    public boolean handleMedicalRequest(String pat_name,String pat_surname,String doc_name, String doc_surname, String date, int approved) throws SQLException{
-        Patient pat = new Patient(pat_name, pat_surname);
-        Doctor doc = new Doctor(doc_name, doc_surname);
+    public boolean handleMedicalRequest(Patient patient,Doctor doctor, String date, int approved) throws SQLException{
+        int patCode=patient.getIdCode();
+        int docCode=doctor.getIdCode();
         
-        if(pat.getIdCode() == 0 || doc.getIdCode() == 0)
+        if(patCode == 0 || docCode == 0)
             return false;
        
         
         CallableStatement cs = Interface.connection.prepareCall("{CALL handle_medical_request(?,?,?,?)}");
-        cs.setInt(1, pat.getIdCode());
-        cs.setInt(2, doc.getIdCode());
+        cs.setInt(1, patCode);
+        cs.setInt(2, docCode);
         cs.setString(3,date);
         cs.setInt(4,approved);
         ResultSet rs = cs.executeQuery();
@@ -76,17 +76,17 @@ public class Employee extends User{
     }
     
     //approved = 0 -> rejected, else approved
-    public boolean handleDeleteRequest(String pat_name,String pat_surname,String doc_name, String doc_surname, String date, int approved) throws SQLException{
-        Patient pat = new Patient(pat_name, pat_surname);
-        Doctor doc = new Doctor(doc_name, doc_surname);
-        
-        if(pat.getIdCode() == 0 || doc.getIdCode() == 0)
+    public boolean handleDeleteRequest(Patient patient,Doctor doctor, String date, int approved) throws SQLException{
+        int patCode=patient.getIdCode();
+        int docCode=doctor.getIdCode();
+
+        if(patCode == 0 || docCode == 0)
             return false;
        
-        
+       
         CallableStatement cs = Interface.connection.prepareCall("{CALL handle_delete_request(?,?,?,?)}");
-        cs.setInt(1, pat.getIdCode());
-        cs.setInt(2, doc.getIdCode());
+        cs.setInt(1, patCode);
+        cs.setInt(2, docCode);
         cs.setString(3,date);
         cs.setInt(4,approved);
         ResultSet rs = cs.executeQuery();
@@ -98,16 +98,15 @@ public class Employee extends User{
     }
     
     //approved = 0 -> rejected, else approved
-    public boolean handleMoveRequest(String pat_name,String pat_surname,String doc_name, String doc_surname, String old_date, int approved) throws SQLException{
-        Patient pat = new Patient(pat_name, pat_surname);
-        Doctor doc = new Doctor(doc_name, doc_surname);
-        
-        if(pat.getIdCode() == 0 || doc.getIdCode() == 0)
+    public boolean handleMoveRequest(Patient patient,Doctor doctor, String old_date, int approved) throws SQLException{
+        int patCode=patient.getIdCode();
+        int docCode=doctor.getIdCode();
+        if(patCode == 0 || docCode == 0)
             return false;
         
         CallableStatement cs = Interface.connection.prepareCall("{CALL handle_move_request(?,?,?,?)}");
-        cs.setInt(1, pat.getIdCode());
-        cs.setInt(2, doc.getIdCode());
+        cs.setInt(1, patCode);
+        cs.setInt(2, docCode);
         cs.setString(3,old_date);
         cs.setInt(4,approved);
         ResultSet rs = cs.executeQuery();
@@ -120,6 +119,7 @@ public class Employee extends User{
     
     public void printMedicalRequests() throws SQLException{
         PreparedStatement ps = Interface.connection.prepareStatement(
+
                            "select d.name, d.surname, p.name, p.surname, m.medical_date"
                          + "medical m inner join doctor d on m.fk_doctor = d.IDCode inner join patient p on m.fk_patient = p.IDCode"
                          + "where approved=false"
@@ -153,19 +153,19 @@ public class Employee extends User{
     }
     
     //pass "" to paramethers you don't want to select by
-    public void printSchedule(String byDocName, String byDocSurname, String byPatName, String byPatSurname, String byDate) throws SQLException{
-        Patient pat = new Patient(byPatName, byPatSurname);
-        Doctor doc = new Doctor(byDocName, byDocSurname);
+    public void printSchedule(Patient patient, Doctor doctor, String byDate) throws SQLException{
+        int patCode=patient.getIdCode();
+        int docCode=doctor.getIdCode();
         
         String query = "select d.name, d.surname, p.name, p.surname, m.medical_date"
                      + "from medical m inner join doctor d on m.fk_doctor = d.IDCode inner join patient p on m.fk_patient = p.IDCode"
                      + "where ";
         boolean previous = false;
-        if(pat.getIdCode() != 0) {
+        if(patCode != 0) {
             query += "m.fk_patient = ?";
             previous = true;
         }
-        if(doc.getIdCode() != 0) {
+        if(docCode != 0) {
             if(previous)
                 query += " and ";
             query += "m.fk_doctor = ?";
@@ -180,11 +180,11 @@ public class Employee extends User{
         
         PreparedStatement ps = Interface.connection.prepareStatement(query);
         int counter = 1;
-        if(pat.getIdCode() != 0) {
-            ps.setInt(counter++, pat.getIdCode());
+        if(patCode != 0) {
+            ps.setInt(counter++, patCode);
         }
-        if(doc.getIdCode() != 0) {
-            ps.setInt(counter++, doc.getIdCode());
+        if(docCode != 0) {
+            ps.setInt(counter++, docCode);
         }
         if(byDate.compareTo("") != 0) {
             ps.setString(counter++, byDate);
