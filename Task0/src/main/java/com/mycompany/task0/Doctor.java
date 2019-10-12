45a8d152f4255a30835a198537cb1c888540eb37
baccio <baccio@localhost>
2019-10-12 19:23:51 +0200

    Doctor ora estende User

--- Task0/src/main/java/com/mycompany/task0/Doctor.java
+++ Task0/src/main/java/com/mycompany/task0/Doctor.java
@@ -6,30 +6,18 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class Doctor {
+public class Doctor extends User{
     private String name;
     private String surname;
     private int id;
 
     public Doctor(String name, String surname) throws SQLException {
-        this.name = name;
-        this.surname = surname;
-
-        int id = 0;
-        ResultSet rs;
-        CallableStatement cst = Start.con.prepareCall("call get_code(?, ?, ?);");
-        cst.setString(1, name);
-        cst.setString(2, surname);
-        cst.setString(3, "doctor");
-
-        rs = cst.executeQuery();
-        rs.next();
-        this.id = rs.getInt(1);
+        super(name, surname, "d");
     }
 
     public Agenda getAgenda(String date) throws SQLException {
         ResultSet rs;
-        CallableStatement cst = Start.con.prepareCall("call get_personal_agenda(?,?);");
+        CallableStatement cst = Interface.connection.prepareCall("call get_personal_agenda(?,?);");
 
         cst.setInt(1,this.id);
         cst.setString(2, date);
