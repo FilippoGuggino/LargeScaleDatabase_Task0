27d938f930021a1bb739ab07a8ada1b2b8177f74
Leonardo <Leonardo@XPS-Leonardo>
2019-10-15 15:38:04 +0200

    Aggiunta javadoc alla classe Patient

--- Task0/src/main/java/com/mycompany/task0/Patient.java
+++ Task0/src/main/java/com/mycompany/task0/Patient.java
@@ -26,12 +26,25 @@ import java.sql.SQLException;
  * @author leocecche
  */
 public class Patient extends User {
-	 
+	
+	/**
+	* constructor which takes and initializes all attributes of the class using
+	* the constructor of superclass User.
+	* @param Name first name of the patient
+	* @param Surname last name of the patient
+	* @throws SQLException if an SQL exception occurred retrieving the related
+	*                      record in the database
+	*/
     public Patient (String Name, String Surname) throws SQLException{
         //uses super class constructor
         super(Name,Surname,"p");
     }
     
+	/**
+	* prints the schedule of the patient in the format Doctor: doctor name surname, date: xxxx
+	* @throws SQLException if an SQL exception occurred retrieving the related
+	*                      record in the database
+	*/
     //prints string of the schedule of the patient in the format Doctor: name surname, date: xxxx
     public void printSchedule() throws SQLException {
     	ResultSet rs;
@@ -43,6 +56,15 @@ public class Patient extends User {
         	System.out.println("Doctor " + rs.getString(1) + " " + rs.getString(2) + ",date " + rs.getString(3));
         }
     }
+    
+	/**
+	* inserts a new medical request for the patient with the specified doctor
+	* @param doctor doctor that is requested by the patient
+	* @param date date that is requested by the patient
+	* @throws SQLException if an SQL exception occurred retrieving the related
+	*                      record in the database
+	* @return true if request is correctly inserted, false if doctor does not exists or something went wrong
+	*/
     
     //returns true if everything was ok, false if it fails
     public boolean newMedicalRequest (Doctor doctor, String date) throws SQLException{
@@ -63,6 +85,15 @@ public class Patient extends User {
         return false;
     }
     
+	/**
+	* inserts a new request of cancellation for the specified medical request
+	* @param doctor doctor that is requested by the patient
+	* @param date date of the specified medical request that is asked to be deleted
+	* @throws SQLException if an SQL exception occurred retrieving the related
+	*                      record in the database
+	* @return true if request is correctly inserted, false if doctor does not exists or something went wrong
+	*/
+    
     //returns true if everything was ok, false if it fails
     public boolean deleteMedicalRequest(Doctor doctor,String date) throws SQLException{
         if(doctor == null || doctor.getIdCode() == 0)
@@ -82,8 +113,18 @@ public class Patient extends User {
         return false;
     }
     
+	/**
+	* inserts a new request of cancellation for the specified medical request
+	* @param doctor doctor that is requested by the patient
+	* @param oldDate date of the specified medical request that is asked to be moved
+	* @param newDate new date for the medical request
+	* @throws SQLException if an SQL exception occurred retrieving the related
+	*                      record in the database
+	* @return true if request is correctly inserted, false if doctor does not exists or something went wrong
+	*/
+    
     //returns true if everything was ok, false if it fails
-    public boolean deleteMedicalRequest(Doctor doctor,String oldDate,String newDate) throws SQLException{
+    public boolean newMoveRequest(Doctor doctor,String oldDate,String newDate) throws SQLException{
        	if(doctor == null || doctor.getIdCode() == 0)
             return false;
         int docCode = doctor.getIdCode();
